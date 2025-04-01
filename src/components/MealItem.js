97dad67488964d96e61a9728ad560c86d3bf6f3c
fredy <fredy-andri.mottus@voco.ee>
2025-04-01 10:33:32 +0300

    yl 5 lahendatud

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,4 +1,10 @@
 const MealItem = ({ meal }) => {
+
+    const formattedPrice = new Intl.NumberFormat('et-EE', {
+        style: 'currency',
+        currency: 'EUR',
+      }).format(meal.price);
+
     return (
       <li className="meal-item">
         <article>
@@ -6,7 +12,7 @@ const MealItem = ({ meal }) => {
           <div>
             <h3>{meal.name}</h3>
             <p className="meal-item-description">{meal.description}</p>
-            <p className="meal-item-price">{meal.price} €</p>
+            <p className="meal-item-price">{formattedPrice}</p>
           </div>
           <div className="meal-item-actions">
             <button className="button">Add to Cart</button>
